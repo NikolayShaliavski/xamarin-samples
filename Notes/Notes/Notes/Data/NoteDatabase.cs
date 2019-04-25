77c6f3db95f0a6925b37105b988808bc81e0d8a5
Nikolay Shalyavski <nikolay.shalyavski@atia.com>
2019-04-25 10:29:22 +0300

    Extended 'Notes' project to use SQLite database.

---
+++ Notes/Notes/Notes/Data/NoteDatabase.cs
@@ -0,0 +1,40 @@
+﻿using Notes.Models;
+using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Notes.Data
+{
+    public class NoteDatabase
+    {
+        private readonly SQLiteAsyncConnection database;
+
+        public NoteDatabase(string dbPath)
+        {
+            this.database = new SQLiteAsyncConnection(dbPath);
+            this.database.CreateTableAsync<Note>().Wait();
+        }
+        public Task<List<Note>> GetNotesAsync()
+        {
+            return this.database.Table<Note>().ToListAsync();
+        }
+        public Task<Note> GetNoteAsync(int id)
+        {
+            return this.database.Table<Note>().
+                                 Where(n => n.ID == id).
+                                 FirstOrDefaultAsync();
+        }
+        public Task<int> SaveNoteAsync(Note note)
+        {
+            if (note.ID != 0)
+            {
+                return this.database.UpdateAsync(note);
+            }
+            return this.database.InsertAsync(note);
+        }
+        public Task<int> DeleteNoteAsync(Note note)
+        {
+            return this.database.DeleteAsync(note);
+        }
+    }
+}
--- Notes/Notes/Notes/NoteEntryPage.xaml.cs
+++ Notes/Notes/Notes/NoteEntryPage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Notes.Models;
 using System;
-using System.IO;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -18,29 +17,15 @@ namespace Notes
         private async void OnSaveButtonClicked(object sender, EventArgs e)
         {
             Note note = base.BindingContext as Note;
-
-            if (string.IsNullOrWhiteSpace(note.Filename))
-            {
-                // Save
-                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
-                File.WriteAllText(filename, note.Text);
-            }
-            else
-            {
-                // Update
-                File.WriteAllText(note.Filename, note.Text);
-            }
+            note.Date = DateTime.UtcNow;
+            await App.Database.SaveNoteAsync(note);
 
             await Navigation.PopAsync();
         }
         private async void OnDeleteButtonClicked(object sender, EventArgs e)
         {
-            var note = (Note)BindingContext;
-
-            if (File.Exists(note.Filename))
-            {
-                File.Delete(note.Filename);
-            }
+            Note note = base.BindingContext as Note;
+            await App.Database.DeleteNoteAsync(note);
 
             await Navigation.PopAsync();
         }
